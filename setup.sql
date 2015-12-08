b364569407d8e49607dba62323d864b2f40de457
marco <t3chn0m4g3@gmail.com>
2015-12-08 12:27:26 +0100

    Kippo is not part of T-Pot 16.03

--- setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE kippo;
-GRANT ALL ON kippo.* TO 'kippo'@'172.16.0.0/255.240.0.0' IDENTIFIED BY 's0m3Secr3T!';
-GRANT ALL ON kippo.* TO 'kippo'@'localhost' IDENTIFIED BY 's0m3Secr3T!';
-FLUSH PRIVILEGES;
-USE kippo;
-SOURCE /opt/kippo/doc/sql/mysql.sql;
